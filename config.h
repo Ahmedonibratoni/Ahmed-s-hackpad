31eb4ab2274b6f6d1609b1e79fbe700831807488
Ahmedonibratoni <121343458+Ahmedonibratoni@users.noreply.github.com>
2026-03-07 12:39:30 +0100

    Add files via upload

---
+++ config.h
@@ -0,0 +1,12 @@
+#define RP2040_BOOTLOADER_DOUBLE_RESET
+#pragma once
+#include "config_common.h"
+#define KEY0 GP1
+#define KEY1 GP2
+#define KEY2 GP4
+#define KEY3 GP3
+#define KEY4 GP7
+#define KEY5 GP0
+#define ENCODERS_PAD_A { GP6 }
+#define ENCODERS_PAD_B { GP7 }
+#define ENCODER_RESOLUTION 4
---
+++ macropad6.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "quantum.h"
+
+#define LAYOUT( \
+K0, K1, K2, \
+K3, K4, K5 \
+) { K0, K1, K2, K3, K4, K5 }
